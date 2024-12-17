785093840ecbad59d4eaf903b8d68bd8fad8316d
Mubarak Afolabi <mubarakbolu150@gmail.com>
2024-12-17 23:09:24 +0100

    Fibonacci with iteration and recursion method

---
+++ fibonacci.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+function fibs(num) {
+  const fibArr = [0, 1];
+
+  for (let i = 2; i < num; i++) {
+    fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+  }
+  console.log(fibArr);
+}
+
+const fibArr = [0, 1];
+let fibNum = 2;
+
+function fibsRec(num) {
+  if (num === fibArr.length || num <= 2) {
+    return fibArr;
+  } else {
+    fibArr.push(fibArr[fibNum - 1] + fibArr[fibNum - 2]);
+    fibNum++;
+    return fibsRec(num);
+  }
+}
