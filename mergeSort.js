55b1d733edf141c3c7310c94bd30839a586290eb
Mubarak Afolabi <mubarakbolu150@gmail.com>
2024-12-24 21:51:05 +0100

    Tested the merge sort function

--- mergeSort.js
+++ mergeSort.js
@@ -27,3 +27,6 @@ const mergeArr = (left, right) => {
 
   return sortedArr.concat(...left).concat(...right);
 };
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
